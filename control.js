603c14513642092e96b78f280f1a8c88e3639e68
Franziska Kissling <frankissling@yahoo.co.uk>
2023-03-29 15:09:53 +0100

    Winning Message Works

--- control.js
+++ control.js
@@ -1,13 +1,24 @@
-
+const squareIdArray = ["#gridSquare1", "#gridSquare2", "#gridSquare3", "#gridSquare4", "#gridSquare5", "#gridSquare6", "#gridSquare7", "#gridSquare8", "#gridSquare9"]
+const row1=[squareIdArray[0], squareIdArray[1], squareIdArray[2]];
+const row2=[squareIdArray[3], squareIdArray[4], squareIdArray[5]];
+const row3=[squareIdArray[6], squareIdArray[7], squareIdArray[8]];
+const col1=[squareIdArray[0], squareIdArray[3], squareIdArray[6]];
+const col2=[squareIdArray[1], squareIdArray[4], squareIdArray[7]];
+const col3=[squareIdArray[2], squareIdArray[5], squareIdArray[8]];
+const dia1=[squareIdArray[0], squareIdArray[4], squareIdArray[8]];
+const dia2=[squareIdArray[2], squareIdArray[4], squareIdArray[6]];
+const lines=[row1, row2, row3, col1, col2, col3, dia1, dia2]
 const grid = document.querySelector(".grid");
 const intro = document.querySelector(".intro");
 const winText = document.querySelector(".winText");
 let gridSquare = "";
 let squareId = "";
-const squareIdArray = ["#gridSquare1", "#gridSquare2", "#gridSquare3", "#gridSquare4", "#gridSquare5", "#gridSquare6", "#gridSquare7", "#gridSquare8", "#gridSquare9"]
+
 let turn = "Player1";
 let won = false;
+let winner=""
 let draw = false;
+let tempIcon=[];
 
 //Create pseudoelements for grid
 for (i = 0; i < 9; i++) {
@@ -40,7 +51,8 @@ function placeTile(tile) {
         selectedTile.innerText = "X";
     }
     else { selectedTile.innerText = "Y" }
-    playerSwitch()
+    playerSwitch();
+    getWinMatrix();
 }
 //convert from player 1 to player 2 and vice versa
 function playerSwitch() {
@@ -54,14 +66,33 @@ function playerSwitch() {
 }
 
 
-console.log(squareIdArray.slice(0, 3))
-let row1 = squareIdArray.slice(0, 3);
-let row2 = squareIdArray.slice(3, 3);
-let row3 = squareIdArray.slice(6, 3);
-console.log(row1, row2, row3);
-let col1=[squareIdArray[0], squareIdArray[3], squareIdArray[6]];
-console.log("col1", col1)
-console.log(squareIdArray[0])
+
+
+//populates arrays where winner can be checked via the checkWinner function
+function getWinMatrix(){
+for(i=0; i<lines.length; i++){
+    for(j=0; j<3; j++){
+       // console.log("linesij", lines[i][j], document.querySelector(lines[i][j]).innerText)
+        
+        tempIcon.push(document.querySelector(lines[i][j]).innerText);
+    console.log(tempIcon)}
+    checkWinner();
+tempIcon=[]
+}
+
+}
+
+function checkWinner(){
+    if(tempIcon.every((val, i, arr)=>val===arr[0]==true)&&!tempIcon[0]==""&&tempIcon.length==3){
+        if (tempIcon[0]==="X"){winner="Player1"; 
+        winText.innerText = "Player 1 wins!!!"}     
+        if(tempIcon[0]=="Y"){winner="Player2";
+        winText.innerText = "Player 2 wins!!!"}
+    }
+}
+
+console.log("lines", lines)
+//console.log(squareIdArray[0])
 
 //for i in [row1, row2, row3]
 //for j in row1[j] if j.value==x then
